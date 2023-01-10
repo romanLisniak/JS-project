f6427fd9817fd41399c77b4b79badf68da0ce1f6
Roman <lesnyakromaa@gmail.com>
2023-01-10 12:08:11 +0100

    secund comit JS

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,19 @@
-const userName = 5;
-let userName = 25;
+const numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
 
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt ('На сколько оцените его?', ''),
+        c = prompt('Один из последних просмотренных фильмов?', ''),
+        d = prompt ('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB)
